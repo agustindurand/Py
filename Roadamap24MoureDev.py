1ae34685fa195fb6def1a49f1361b5696881ee4c
Agustin <agustindurand.98@gmail.com>
2024-03-12 14:44:24 -0300

    doc

---
+++ Roadamap24MoureDev.py
@@ -0,0 +1,28 @@
+""" 1 EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!" 
+ """ 
+
+#Comentario de insercion de URL https://www.python.org/
+#Comentario Modo Ejemplo Numero 1 
+""""Comentario Modo Ejemplo
+    Numero 2"""
+Variable = True
+Variable2 = 1,5
+Variable3 = "String"
+Variable4 = 100 
+Variable5 = False
+CONSTANTE_NRO1 = 120
+print("Hello Python")
+
+print(type(Variable))
+print(type(Variable2))
+print(type(Variable3))
+print(type(Variable4))
+print(type(Variable5))
