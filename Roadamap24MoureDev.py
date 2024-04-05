f24681bb4156289edfd1036c50c84f1416b241c1
Agustin <agustindurand.98@gmail.com>
2024-04-05 12:24:40 -0300

    roadmap and doc

--- Roadamap24MoureDev.py
+++ Roadamap24MoureDev.py
@@ -1,4 +1,4 @@
-""" 1 EJERCICIO:
+""" 00 EJERCICIO:
  * - Crea un comentario en el código y coloca la URL del sitio web oficial del
  *   lenguaje de programación que has seleccionado.
  * - Representa las diferentes sintaxis que existen de crear comentarios
@@ -29,7 +29,7 @@ print(type(Variable5))
 
 
 """"
- * EJERCICIO:
+ * 01 EJERCICIO:
  * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
  *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
  *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
@@ -47,5 +47,92 @@ print(type(Variable5))
  */  """ 
  
  
-
  
+#Operadores Aritméticos:
+
+numero_ejemplo = 12 
+numero_ejemplo2 = 14
+
+numero_ejemplo + numero_ejemplo2
+numero_ejemplo - numero_ejemplo2
+numero_ejemplo * numero_ejemplo2
+numero_ejemplo / numero_ejemplo2
+numero_ejemplo % numero_ejemplo2
+numero_ejemplo ** numero_ejemplo2
+numero_ejemplo // numero_ejemplo2
+
+# Operadores Lógicos:
+
+numero_ejemplo > 10 and numero_ejemplo2 > 10 
+numero_ejemplo < 10 or numero_ejemplo2 < 10 
+resultado = not (numero_ejemplo < numero_ejemplo2)
+
+
+
+numero_ejemplo == numero_ejemplo2 #False
+numero_ejemplo != numero_ejemplo2 #True
+
+numero_ejemplo >= numero_ejemplo2 
+numero_ejemplo <= numero_ejemplo2
+
+
+#Operadores de Asignación:
+
+numero_ejemplo3 = 12000
+numero_ejemplo3 += numero_ejemplo
+numero_ejemplo2 -= numero_ejemplo 
+numero_ejemplo2 *= numero_ejemplo
+numero_ejemplo2 /= numero_ejemplo
+
+#Operadores de Identidad:
+
+lista1 = [1, 2, 3]
+lista2 = [1, 2, 3]
+lista1 is lista2 #False
+lista1 is not lista2 
+
+#Operadores de Pertenencia:
+
+tupla = ("elemento1", "elemento2", "elemento3", "elemento4")
+"elemento3" in tupla #True 
+
+"elemento5" not in tupla #True 
+ 
+#Estructuras de Control Condicional
+numero_condicional = 10
+if numero_condicional > 0:
+    print("El número es positivo.")
+elif numero_condicional < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
+
+
+#Estructura de Control Iterativa
+
+for i in range(1, 10): #la variable i se declara simultaneamente a la declaracion del For
+    print(f"Este es el número que se obtiene mediante la estructura de control Iterativa For {i}")
+    
+contador = 0
+while contador < 5:
+    print(f"El contador es: {contador}")
+    contador += 1
+    
+for x in range(1, 11):
+    if x == 5:
+        continue  # Salta la iteración actual si el número es 5
+    elif x == 8:
+        break  # Sale del bucle si el número es 8
+    else:
+        pass  # No hace nada, solo pasa al siguiente paso
+    print(f"Impresion de numero con estructura Continue, Break y Pass {x}")
+
+
+#No hay Switch en Python 
+
+#EJERCICIO
+for numero_ejercicio in range (10,55):
+    if numero_ejercicio % 2 == 0 and numero_ejercicio != 16 and numero_ejercicio % 3 != 0:
+        print(numero_ejercicio)
+        
+        
