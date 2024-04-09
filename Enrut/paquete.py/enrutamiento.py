c6efbd64f7a62ba8ca60b92f193149e6d84d1fad
Agustin <agustindurand.98@gmail.com>
2024-04-09 18:30:13 -0300

    enrutamiento, modulo

---
+++ Enrut/paquete.py/enrutamiento.py
@@ -0,0 +1,7 @@
+def suma_cuadrados(a, b):
+    cuadrado_a = a ** 2
+    cuadrado_b = b ** 2
+    suma = cuadrado_a + cuadrado_b
+    return suma
+
+print (__name__) #Imprime el nombre de esta carpeta y archivo en la consola cuando la importemos desde otro archivo
--- Roadamap24MoureDev.py
+++ Roadamap24MoureDev.py
@@ -216,3 +216,23 @@ def funcion_retorna_numeros (a,b):
   
 contador = funcion_retorna_numeros("Ventaja del 3", "Ventaja del 5")
 print("Número de veces que se ha impreso el número:", contador)
+
+"""" 
+ * EJERCICIO:
+ * - Muestra ejemplos de creación de todas las estructuras soportadas por defecto
+ *   en tu lenguaje.
+ * - Utiliza operaciones de inserción, borrado, actualización y ordenación.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización
+ *   y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar,
+ *   y a continuación los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no númericos y con más
+ *   de 11 dígitos (o el número de dígitos que quieras).
+ * - También se debe proponer una operación de finalización del programa.
+ */
+ 
+ """
